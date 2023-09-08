e562ab8fe3a84f160a5791cfbc8d7820d7d6f937
gabrielosantosb <gabrielosantosb@gmail.com>
2023-09-08 17:22:02 -0300

    crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { AppRoutingModule } from './app-routing.module'; // Importe o AppRoutingModule aqui
+import { HttpClientModule } from '@angular/common/http';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import {FormsModule} from "@angular/forms"; // Importe o HttpClientModule aqui
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CreateUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
